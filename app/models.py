2dbf70e7ed5f1cee858484457a31d1614622de13
jdkanuch <jdkanuch@gmail.com>
2019-08-13 08:30:21 -0500

    Defined basic db schema.

--- app/models.py
+++ app/models.py
@@ -1,2 +1,15 @@
-from flask_sqlalchemy import SQLAlchemy
+# This module defines the DB Schema
+
+from app import db
+
+
+# Model Definitions
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), index=True, nullable=False)
+    email = db.Column(db.String(120), index=True, nullable=False)
+    password_hash = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User {self.username}"
 
