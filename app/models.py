7929dd35250f1aeb71649122c6a39832e28f82e0
jdkanuch <jdkanuch@gmail.com>
2019-08-13 08:38:38 -0500

    Instanced the db.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 # This module defines the DB Schema
 # Once flask-migrations is installed, can run 'flask db init' in terminal
 # to set up migrations
+# 'flask db migrate' will generate automatic migration scripts (and initialize db if not existing)
+# You need to run 'flask db upgrade' to apply the migration
+# You can run 'flask db downgrade' to revert to the previous migration
 
 from app import db
 
